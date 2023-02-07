b16da703c44c5d49a72801b95ceb974ff25db8bb
Sayuk <sayukbohdan@gmail.com>
2023-02-07 21:33:29 +0200

    07

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+export function fetchCountries(name) {
+    const BASE_URL = 'https://restcountries.com/v3.1/name/';
+    const fields = 'fields=name,capital,population,flags,languages';
+    return fetch(`${BASE_URL}${name}?${fields}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json()
+        });
+}
+fetchCountries().then(data => console.log(data))
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
 
 const DEBOUNCE_DELAY = 300;
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryinfo = document.querySelector('.country-info');
+
+countryInput.addEventListener('inpyt', debounce(onCountryInput, DEBOUNCE_DELAY))
