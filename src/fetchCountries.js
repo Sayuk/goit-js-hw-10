d4a6e5596753b69f045b05ef8bf72b84d49ca95a
Sayuk <sayukbohdan@gmail.com>
2023-02-09 23:12:37 +0200

    6

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,15 @@
+
+
 export function fetchCountries(name) {
-    const BASE_URL = 'https://restcountries.com/v3.1/name/';
-    const fields = 'fields=name,capital,population,flags,languages';
-    return fetch(`${BASE_URL}${name}?${fields}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json()
-        });
+  const BASE_URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+    return fetch(BASE_URL)
+        .then(resp => {
+            
+                if (!resp.ok) {
+                    throw new Error(resp.statusText);
+                }
+                return resp.json();
+            });
+
 }
 fetchCountries().then(data => console.log(data))
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
-import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
+import './css/styles.css'
+import debounce from 'lodash.debounce'
+import { fetchCountries } from './fetchCountries'
+import Notiflix from 'notiflix'
 
 
 const DEBOUNCE_DELAY = 300;
@@ -9,4 +9,12 @@ const countryInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryinfo = document.querySelector('.country-info');
 
-countryInput.addEventListener('inpyt', debounce(onCountryInput, DEBOUNCE_DELAY))
+countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY))
+function onCountryInput() {
+    const nameCountry = countryInput.value.trim()
+    if (nameCountry === '') {
+        countryList.innerHTML = '';
+        countryinfo.innerHTML = '';
+        return;
+    }
+}
