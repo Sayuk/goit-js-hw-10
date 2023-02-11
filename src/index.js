02db1161909fdadaf2baa3fd144356c9ca365a34
Sayuk <sayukbohdan@gmail.com>
2023-02-11 23:59:42 +0200

    S

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import debounce from 'lodash.debounce';
 
-import fetchCountries from './fetchCountries';
-
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
 const DEBOUNCE_DELAY = 300;
