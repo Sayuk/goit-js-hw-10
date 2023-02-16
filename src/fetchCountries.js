2cb7650f25ea192117a405022baa68f290990cde
Sayuk <sayukbohdan@gmail.com>
2023-02-16 23:31:38 +0200

    3

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
 const BASE_URL = 'https://restcountries.com/v3.1/?name.official&capital&population&flags.svg&languages'
-const fields = 'fields=name,capital,population,flags,languages'
+
 export function fetchCountries(name) {
   
-    return fetchCountries(`${BASE_URL}`)
+    return fetch(`${BASE_URL}`)
         .then(resp => {
             
 
@@ -14,4 +14,4 @@ export function fetchCountries(name) {
             });
 
 }
-fetchCountries().then(data => console.log(data))
+
