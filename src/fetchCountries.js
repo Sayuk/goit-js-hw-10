df3929241b8502aec908749b653f362bbefa8b28
Sayuk <sayukbohdan@gmail.com>
2023-02-18 20:01:01 +0200

    e

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,18 +1,24 @@
-const BASE_URL = 'https://restcountries.com/?name.official&capital&population&flags.svg&languages'
+const BASE_URL = 'https://restcountries.com/name/'
 
-export function fetchCountries(name) {
+// export function fetchCountries(name) {
   
-    return fetch(`${BASE_URL}`)
-        .then(resp => {
+//     return fetch(`${BASE_URL}`)
+//         .then(resp => {
             
 
 
-                if (!resp.ok) {
-                    throw new Error(resp.statusText);
-                }
-                return resp.json();
-            });
+//                 if (!resp.ok) {
+//                     throw new Error(resp.statusText);
+//                 }
+//                 return resp.json();
+//             });
 
+// }
+
+const fields = 'fields=name,capital,population,flags,languages'
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}?${fields}`)
+    .then(response => response.json())
+    .catch(error => console.log(error))
 }
-
-// ?name.official&capital&population&flags.svg&languages
+// ?name.official&capital&population&flags.svg&languages'
