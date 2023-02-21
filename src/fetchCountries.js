028b7a93daf512d73aea9de277af4653ab728ed4
Sayuk <sayukbohdan@gmail.com>
2023-02-21 20:16:22 +0200

    1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,9 @@
 const BASE_URL = 'https://restcountries.com/?name.official&capital&population&flags.svg&languages'
+const key ='fields=name,capital,population,flags,languages'
 
 export function fetchCountries(name) {
   
-    return fetch(`${BASE_URL}`)
+    return fetch(`${BASE_URL}${name}?${fields}`)
         .then(resp => {
             
 
